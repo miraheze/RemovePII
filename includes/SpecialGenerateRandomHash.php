a5ba21523787cdb47e424743cbc9e9944bc79f6b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-11 15:24:09 -0600

    Update SpecialGenerateRandomHash.php

--- includes/SpecialGenerateRandomHash.php
+++ includes/SpecialGenerateRandomHash.php
@@ -1,14 +1,46 @@
 <?php
-class SpecialGenerateRandomHash extends SpecialPage {
-	public function __construct() {
-		parent::__construct( 'GenerateRandomHash' );
+
+class SpecialGenerateRandomHash extends FormSpecialPage {
+    
+	private $config;
+    
+	public function __construct( ConfigFactory $config ) {
+		parent::__construct( 'GenerateRandomHash', 'generate-random-hash' );
+		
+		$this->config = $config->makeConfig( 'RemovePII' );
+	}
+	
+	protected function getFormFields() {
+		$formDescriptor = [];
+
+		$formDescriptor['HashLength'] = [
+			'type' => 'int',
+			'default' => 32,
+			'required' => true,
+			'help-message' => 'hash-length-help',
+		];
+
+		if ( $this->config->get( 'RemovePIIHashPrefixOptions' ) ) {
+			$formDescriptor['HashPrefix'] = [
+				'type' => 'select',
+				'options' => $this->config->get( 'RemovePIIHashPrefixOptions' ),
+				'required' => true,
+				'help-message' => 'hash-prefix-help',
+			];
+		}
+		
+		return $formDescriptor;
 	}
 
-	public function execute( $subPage ) {
+	public function onSubmit( array $formData ) {
 		$output = $this->getOutput();
 		$this->setHeaders();
 
-		$generatedHash = 'MirahezeGDPR_' . substr( sha1( random_bytes( 10 ) ), 0, 32 );
-		$output->addHTML( $generatedHash );
+		$generatedHash = $formData['HashPrefix'] . substr( sha1( random_bytes( 10 ) ), 0, $formData['HashLength'] );
+		$output->addHTML( Html::successBox( $generatedHash ) );
+	}
+	
+	protected function getDisplayFormat() {
+		return 'ooui';
 	}
 }

738036fa9c7bd5d572898a4104b9d20d324481b8
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-13 14:20:42 -0600

    Create SpecialGenerateRandomHash (T7286: phase one) (#1)

---
+++ includes/SpecialGenerateRandomHash.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Miraheze\RemovePII;
+
+use ConfigFactory;
+use FormSpecialPage;
+use Html;
+
+class SpecialGenerateRandomHash extends FormSpecialPage {
+	/** @var ConfigFactory */
+	private $config;
+
+	/**
+	 * @param ConfigFactory $configFactory
+	 */
+	public function __construct( ConfigFactory $configFactory ) {
+		parent::__construct( 'GenerateRandomHash', 'generate-random-hash' );
+
+		$this->config = $configFactory->makeConfig( 'RemovePII' );
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getFormFields() {
+		$formDescriptor = [];
+
+		$formDescriptor['HashLength'] = [
+			'type' => 'int',
+			'default' => 32,
+			'max' => 32,
+			'min' => 8,
+			'required' => true,
+			'help-message' => 'hash-length-help',
+		];
+
+		if ( $this->config->get( 'RemovePIIHashPrefixOptions' ) ) {
+			$formDescriptor['HashPrefix'] = [
+				'type' => 'select',
+				'options' => $this->config->get( 'RemovePIIHashPrefixOptions' ),
+				'required' => true,
+				'help-message' => 'hash-prefix-help',
+			];
+		}
+
+		return $formDescriptor;
+	}
+
+	/**
+	 * @param array $formData
+	 */
+	public function onSubmit( array $formData ) {
+		$out = $this->getOutput();
+
+		$generatedHash = ( $formData['HashPrefix'] ?? $this->config->get( 'RemovePIIHashPrefix' ) ) . substr( sha1( random_bytes( 10 ) ), 0, $formData['HashLength'] );
+		$out->addHTML( Html::successBox( $generatedHash ) );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getDisplayFormat() {
+		return 'ooui';
+	}
+}

0ef6ae8c7d938b4189860a117b8fbf4df0328466
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-17 18:01:11 -0600

    Update SpecialRemovePII.php

--- includes/SpecialRemovePII.php
+++ includes/SpecialRemovePII.php
@@ -16,6 +16,7 @@ use Html;
 use JobQueueGroup;
 use MediaWiki\User\UserGroupManager;
 use Status;
+use User;
 
 class SpecialRemovePII extends FormSpecialPage {
 	/** @var Config */
@@ -63,6 +64,16 @@ class SpecialRemovePII extends FormSpecialPage {
 			'required' => true,
 			'label-message' => 'removepii-newname-label'
 		];
+		
+		$formDescriptor['action'] = [
+			'type' => 'select',
+			'options' => [
+				'RemovePII', => 'removepii',
+				'Rename User' => 'rename'
+			],
+			'required' => true,
+			'label-message' => 'removepii-action-label'
+		];
 
 		return $formDescriptor;
 	}
@@ -99,102 +110,116 @@ class SpecialRemovePII extends FormSpecialPage {
 	public function onSubmit( array $formData ) {
 		$out = $this->getOutput();
 
-		$valid = $this->validate( $formData );
-		if ( !$valid->isOK() ) {
-			return $valid;
-		}
+		if ( $formData['action'] === 'rename' ) {
+			$valid = $this->validate( $formData );
+			if ( !$valid->isOK() ) {
+				return $valid;
+			}
 
-		$oldUser = User::newFromName( $formData['oldname'] );
-		$newUser = User::newFromName( $formData['newname'], 'creatable' );
+			$oldUser = User::newFromName( $formData['oldname'] );
+			$newUser = User::newFromName( $formData['newname'], 'creatable' );
 
-		$session = $this->getContext()->exportSession();
-		$globalRenameUser = new GlobalRenameUser(
-			$this->getUser(),
-			$oldUser,
-			CentralAuthUser::getInstance( $oldUser ),
-			$newUser,
-			CentralAuthUser::getInstance( $newUser ),
-			new GlobalRenameUserStatus( $newUser->getName() ),
-			'JobQueueGroup::singleton',
-			new GlobalRenameUserDatabaseUpdates(),
-			new GlobalRenameUserLogger( $this->getUser() ),
-			$session
-		);
-
-		$globalRenameUser->rename( $formData );
-
-		$jobParams = [
-			'oldName' => $formData['oldname'],
-			'newName' => $formData['newname'],
-		];
-
-		$oldCentral = CentralAuthUser::getInstanceByName( $formData['oldname'] );
-
-		$newCentral = CentralAuthUser::getInstanceByName( $formData['newname'] );
-
-		if ( $oldCentral->renameInProgress() ) {
-			$out->addHTML(
-				Html::errorBox(
-					$this->msg( 'centralauth-renameuser-global-inprogress', $formData['oldname'] )
-				)
+			$session = $this->getContext()->exportSession();
+			$globalRenameUser = new GlobalRenameUser(
+				$this->getUser(),
+				$oldUser,
+				CentralAuthUser::getInstance( $oldUser ),
+				$newUser,
+				CentralAuthUser::getInstance( $newUser ),
+				new GlobalRenameUserStatus( $newUser->getName() ),
+				'JobQueueGroup::singleton',
+				new GlobalRenameUserDatabaseUpdates(),
+				new GlobalRenameUserLogger( $this->getUser() ),
+				$session
 			);
 
-			return false;
-		}
-
-		if ( $newCentral->renameInProgress() ) {
-			$out->addHTML(
-				Html::errorBox(
-					$this->msg( 'centralauth-renameuser-global-inprogress', $formData['newname'] )
-				)
+			$globalRenameUser->rename(
+				array_merge( [
+					'movepages' => true,
+					'suppressredirects' => true,
+					'reason' => null,
+					'force' => true
+				], $formData )
 			);
 
-			return false;
+			return true;
+		} elseif ( $formData['action'] === 'removepii' ) {
+			$jobParams = [
+				'oldName' => $formData['oldname'],
+				'newName' => $formData['newname'],
+			];
+
+			$oldCentral = CentralAuthUser::getInstanceByName( $formData['oldname'] );
+
+			$newCentral = CentralAuthUser::getInstanceByName( $formData['newname'] );
+
+			if ( $oldCentral->renameInProgress() ) {
+				$out->addHTML(
+					Html::errorBox(
+						$this->msg( 'centralauth-renameuser-global-inprogress', $formData['oldname'] )
+					)
+				);
+
+				return false;
+			}
+
+			if ( $newCentral->renameInProgress() ) {
+				$out->addHTML(
+					Html::errorBox(
+						$this->msg( 'centralauth-renameuser-global-inprogress', $formData['newname'] )
+					)
+				);
+
+				return false;
+			}
+
+			if ( !$newCentral->exists() ) {
+				$out->addHTML(
+					Html::errorBox(
+						$this->msg( 'centralauth-admin-status-nonexistent', $formData['newname'] )
+					)
+				);
+
+				return false;
+			}
+
+			// Invalidate cache before we begin transaction
+			$newCentral->invalidateCache();
+
+			// Delay cache invalidation until we finish transaction
+			$newCentral->startTransaction();
+
+			// Run RemovePIIJob on all attached wikis
+			// todo: does this include deleted wikis?
+			foreach ( $newCentral->listAttached() as $database ) {
+				$jobParams['database'] = $database;
+
+				JobQueueGroup::singleton()->push(
+					new RemovePIIJob(
+						null,
+						$jobParams,
+						$this->userGroupManager,
+						$this->getOutput()
+					)
+				);
+			}
+
+			$user = User::newFromName( $formData['newname'] );
+
+			// Remove user email
+			$user->invalidateEmail();
+			$user->saveSettings();
+
+			// Lock global account
+			$newCentral->adminLock();
+
+			// End transaction, enable cache invalidation again
+			$newCentral->endTransaction();
+
+			return true;
 		}
-
-		if ( !$newCentral->exists() ) {
-			$out->addHTML(
-				Html::errorBox(
-					$this->msg( 'centralauth-admin-status-nonexistent', $formData['newname'] )
-				)
-			);
-
-			return false;
-		}
-
-		// Invalidate cache before we begin transaction
-		$newCentral->invalidateCache();
-
-		// Delay cache invalidation until we finish transaction
-		$newCentral->startTransaction();
-
-		// Run RemovePIIJob on all attached wikis
-		// todo: does this include deleted wikis?
-		foreach ( $newCentral->listAttached() as $database ) {
-			$jobParams['database'] = $database;
-
-			JobQueueGroup::singleton()->push(
-				new RemovePIIJob(
-					$this->userGroupManager,
-					$this->getOutput(),
-					$jobParams
-				)
-			);
-		}
-
-		$user = User::newFromName( $formData['newname'] );
-
-		// Remove user email
-		$user->invalidateEmail();
-		$user->saveSettings();
-
-		// Lock global account
-		$newCentral->adminLock();
-
-		// End transaction, enable cache invalidation again
-		$newCentral->endTransaction();
-
-		return true;
+		
+		return false;
 	}
 
 	/**

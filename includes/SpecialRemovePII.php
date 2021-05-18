92ed7592c3e833c55a4bd815837244b77f7f080a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-17 18:23:39 -0600

    Partially implement phase 3

--- includes/SpecialRemovePII.php
+++ includes/SpecialRemovePII.php
@@ -5,10 +5,18 @@ namespace Miraheze\RemovePII;
 use CentralAuthUser;
 use Config;
 use ConfigFactory;
+use ExtensionRegistry;
 use FormSpecialPage;
+use GlobalRenameUser;
+use GlobalRenameUserDatabaseUpdates;
+use GlobalRenameUserLogger;
+use GlobalRenameUserStatus;
+use GlobalRenameUserValidator;
 use Html;
 use JobQueueGroup;
 use MediaWiki\User\UserGroupManager;
+use Status;
+use User;
 
 class SpecialRemovePII extends FormSpecialPage {
 	/** @var Config */
@@ -45,19 +53,54 @@ class SpecialRemovePII extends FormSpecialPage {
 
 		$formDescriptor = [];
 
-		$formDescriptor['oldName'] = [
+		$formDescriptor['oldname'] = [
 			'type' => 'text',
 			'required' => true,
 			'label-message' => 'removepii-oldname-label'
 		];
 
-		$formDescriptor['newName'] = [
+		$formDescriptor['newname'] = [
 			'type' => 'text',
 			'required' => true,
 			'label-message' => 'removepii-newname-label'
 		];
+		
+		$formDescriptor['action'] = [
+			'type' => 'select',
+			'options' => [
+				'RemovePII' => 'removepii',
+				'Rename User' => 'rename'
+			],
+			'required' => true,
+			'label-message' => 'removepii-action-label'
+		];
 
 		return $formDescriptor;
+	}
+
+	public function validate( array $data ) {
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'Renameuser' ) ) {
+			return Status::newFatal( 'centralauth-rename-notinstalled' );
+		}
+
+		$oldUser = User::newFromName( $data['oldname'] );
+		if ( !$oldUser ) {
+			return Status::newFatal( 'centralauth-rename-doesnotexist' );
+		}
+
+		if ( $oldUser->getName() === $this->getUser()->getName() ) {
+			return Status::newFatal( 'centralauth-rename-cannotself' );
+		}
+
+		$newUser = User::newFromName( $data['newname'] );
+		if ( !$newUser ) {
+			return Status::newFatal( 'centralauth-rename-badusername' );
+		}
+
+		$validator = new GlobalRenameUserValidator();
+		$status = $validator->validate( $oldUser, $newUser );
+
+		return $status;
 	}
 
 	/**
@@ -67,88 +110,116 @@ class SpecialRemovePII extends FormSpecialPage {
 	public function onSubmit( array $formData ) {
 		$out = $this->getOutput();
 
-		$jobParams = [
-			'oldName' => $formData['oldName'],
-			'newName' => $formData['newName'],
-		];
+		if ( $formData['action'] === 'rename' ) {
+			$valid = $this->validate( $formData );
+			if ( !$valid->isOK() ) {
+				return $valid;
+			}
 
-		$oldCentral = CentralAuthUser::getInstanceByName( $formData['oldName'] );
+			$oldUser = User::newFromName( $formData['oldname'] );
+			$newUser = User::newFromName( $formData['newname'], 'creatable' );
 
-		$newCentral = CentralAuthUser::getInstanceByName( $formData['newName'] );
-
-		if ( $oldCentral->renameInProgress() ) {
-			$out->addHTML(
-				Html::errorBox(
-					$this->msg( 'centralauth-renameuser-global-inprogress', $formData['oldName'] )
-				)
+			$session = $this->getContext()->exportSession();
+			$globalRenameUser = new GlobalRenameUser(
+				$this->getUser(),
+				$oldUser,
+				CentralAuthUser::getInstance( $oldUser ),
+				$newUser,
+				CentralAuthUser::getInstance( $newUser ),
+				new GlobalRenameUserStatus( $newUser->getName() ),
+				'JobQueueGroup::singleton',
+				new GlobalRenameUserDatabaseUpdates(),
+				new GlobalRenameUserLogger( $this->getUser() ),
+				$session
 			);
 
-			return false;
-		}
-
-		if ( $newCentral->renameInProgress() ) {
-			$out->addHTML(
-				Html::errorBox(
-					$this->msg( 'centralauth-renameuser-global-inprogress', $formData['newName'] )
-				)
+			$globalRenameUser->rename(
+				array_merge( [
+					'movepages' => true,
+					'suppressredirects' => true,
+					'reason' => null,
+					'force' => true
+				], $formData )
 			);
 
-			return false;
+			return true;
+		} elseif ( $formData['action'] === 'removepii' ) {
+			$jobParams = [
+				'oldName' => $formData['oldname'],
+				'newName' => $formData['newname'],
+			];
+
+			$oldCentral = CentralAuthUser::getInstanceByName( $formData['oldname'] );
+
+			$newCentral = CentralAuthUser::getInstanceByName( $formData['newname'] );
+
+			if ( $oldCentral->renameInProgress() ) {
+				$out->addHTML(
+					Html::errorBox(
+						$this->msg( 'centralauth-renameuser-global-inprogress', $formData['oldname'] )
+					)
+				);
+
+				return false;
+			}
+
+			if ( $newCentral->renameInProgress() ) {
+				$out->addHTML(
+					Html::errorBox(
+						$this->msg( 'centralauth-renameuser-global-inprogress', $formData['newname'] )
+					)
+				);
+
+				return false;
+			}
+
+			if ( !$newCentral->exists() ) {
+				$out->addHTML(
+					Html::errorBox(
+						$this->msg( 'centralauth-admin-status-nonexistent', $formData['newname'] )
+					)
+				);
+
+				return false;
+			}
+
+			// Invalidate cache before we begin transaction
+			$newCentral->invalidateCache();
+
+			// Delay cache invalidation until we finish transaction
+			$newCentral->startTransaction();
+
+			// Run RemovePIIJob on all attached wikis
+			// todo: does this include deleted wikis?
+			foreach ( $newCentral->listAttached() as $database ) {
+				$jobParams['database'] = $database;
+
+				JobQueueGroup::singleton()->push(
+					new RemovePIIJob(
+						null,
+						$jobParams,
+						$this->userGroupManager,
+						$this->getOutput()
+					)
+				);
+			}
+
+			$user = User::newFromName( $formData['newname'] );
+
+			// Remove user email
+			$user->invalidateEmail();
+			$user->saveSettings();
+
+			// Lock global account
+			$newCentral->adminLock();
+
+			// End transaction, enable cache invalidation again
+			$newCentral->endTransaction();
+
+			return true;
 		}
-
-		if ( !$newCentral->exists() ) {
-			$out->addHTML(
-				Html::errorBox(
-					$this->msg( 'centralauth-admin-status-nonexistent', $formData['newName'] )
-				)
-			);
-
-			return false;
-		}
-
-		// Invalidate cache before we begin transaction
-		$newCentral->invalidateCache();
-
-		// Delay cache invalidation until we finish transaction
-		$newCentral->startTransaction();
-
-		// Run RemovePIIJob on all attached wikis
-		// todo: does this include deleted wikis?
-		foreach ( $newCentral->listAttached() as $database ) {
-			$jobParams['database'] = $database;
-
-			JobQueueGroup::singleton()->push(
-				new RemovePIIJob(
-					$this->userGroupManager,
-					$this->getOutput(),
-					$jobParams
-				)
-			);
-		}
-
-		$dbw = wfGetDB( DB_MASTER, [], $this->config->get( 'CentralAuthDatabase' ) );
-
-		if ( $newCentral->getEmail() ) {
-			$dbw->update(
-				'globaluser',
-				[
-					'gu_email' => ''
-				],
-				[
-					'gu_email' => $newCentral->getEmail(),
-					'gu_name' => $newCentral->getName()
-				],
-				__METHOD__
-			);
-		}
-
-		// Lock global account
-		$newCentral->adminLock();
-
-		// End transaction, enable cache invalidation again
-		$newCentral->endTransaction();
-
-		return true;
+		
+		return false;
 	}
 
 	/**

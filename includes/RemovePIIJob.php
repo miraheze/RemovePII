536d0fe2a5c459293aac0e40d392209d12902f37
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-19 01:02:56 -0600

    Update RemovePIIJob.php

--- includes/RemovePIIJob.php
+++ includes/RemovePIIJob.php
@@ -441,7 +441,7 @@ class RemovePIIJob extends Job implements GenericParameterJob {
 			$title = Title::newFromRow( $row );
 
 			$userPage = WikiPage::factory( $title );
-			$status = $userPage->doDeleteArticleReal( '', $user );
+			$status = $userPage->doDeleteArticleReal( '', $user, false, null, '', [], 'delete', true );
 
 			if ( !$status->isOK() ) {
 				$errorMessage = json_encode( $status->getErrorsByType( 'error' ) );

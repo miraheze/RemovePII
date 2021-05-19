9fb98bdcc5b69baf8cc2bca0247a487ff6ff83c7
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-19 02:16:50 -0600

    Update RemovePIIJob.php

--- includes/RemovePIIJob.php
+++ includes/RemovePIIJob.php
@@ -480,7 +480,7 @@ class RemovePIIJob extends Job implements GenericParameterJob {
 		}
 
 		$logEntry = new ManualLogEntry( 'removepii', 'action' );
-		$logEntry->setPerformer( CentralAuthUser::getInstance( RequestContext::getMain()->getUser() ) );
+		$logEntry->setPerformer( User::newFromName( CentralAuthUser::getInstance( RequestContext::getMain()->getUser() )->getName() ) );
 		$logEntry->setTarget( Title::newFromText( 'RemovePII' , NS_SPECIAL ) );
 		$logID = $logEntry->insert();
 		$logEntry->publish( $logID );

0d0f7a2ff36a68c64ba93f63898d647fcb571fec
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-09 23:20:52 -0600

    Update download-mediawiki.sh

--- .github/workflows/download-mediawiki.sh
+++ .github/workflows/download-mediawiki.sh
@@ -2,8 +2,9 @@
 set -ex
 
 MW_BRANCH=$1
+MW_REPO=$2
 
-wget https://github.com/wikimedia/mediawiki/archive/"$MW_BRANCH".tar.gz -nv
+wget https://github.com/"$MW_REPO"/mediawiki/archive/"$MW_BRANCH".tar.gz -nv
 
 tar -zxf "$MW_BRANCH".tar.gz
 mv mediawiki-"$MW_BRANCH" mediawiki

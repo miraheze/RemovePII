43f07e042689c87999ce4385a897e46ffb3fe7d7
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-09 23:09:10 -0600

    Update download-mediawiki.sh

--- .github/workflows/download-mediawiki.sh
+++ .github/workflows/download-mediawiki.sh
@@ -9,10 +9,5 @@ tar -zxf "$MW_BRANCH".tar.gz
 mv mediawiki-"$MW_BRANCH" mediawiki
 
 cd mediawiki
-git clone https://github.com/wikimedia/mediawiki-extensions-CentralAuth.git --depth=1 --branch="$MW_BRANCH" extensions/CentralAuth
-git clone https://github.com/wikimedia/mediawiki-extensions-BlogPage.git --depth=1 extensions/BlogPage
-git clone https://github.com/wikimedia/mediawiki-extensions-SocialProfile.git --depth=1 extensions/SocialProfile
-
-git clone https://github.com/Universal-Omega/SimpleBlogPage --depth=1 extensions/SimpleBlogPage
 
 composer update --prefer-dist --no-progress

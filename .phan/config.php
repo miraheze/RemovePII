64289135fc91941776392ff03ca5495a40a0cd1d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-23 13:04:01 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -6,7 +6,6 @@ $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
 		'../../extensions/CentralAuth',
 		'../../extensions/SocialProfile',
-		'../../extensions/BlogPage',
 		'../../extensions/SimpleBlogPage',
 	]
 );
@@ -15,7 +14,6 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], [
 		'../../extensions/CentralAuth',
 		'../../extensions/SocialProfile',
-		'../../extensions/BlogPage',
 		'../../extensions/SimpleBlogPage',
 	]
 );

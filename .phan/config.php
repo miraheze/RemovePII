99feedb9f293fe80979c97d2d8dfa39d994a013e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-27 18:07:01 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -20,6 +20,8 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 
 $cfg['suppress_issue_types'] = [
 	'PhanTypeMismatchArgumentNullable',
+	'PhanAccessMethodInternal',
+	'SecurityCheck-LikelyFalsePositive',
 ];
 
 return $cfg;

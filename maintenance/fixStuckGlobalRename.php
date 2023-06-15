19d84f5243bfd66a7fdd27de16817110536a5868
paladox <paladox@users.noreply.github.com>
2023-06-15 14:41:04 +0100

    Fork fixStuckGlobalRename from CentralAuth
    Source: https://github.com/wikimedia/mediawiki-extensions-CentralAuth/blob/a281f25becfb3ae6ef113c20b16ccf5376ab65ea/maintenance/fixStuckGlobalRename.php

---
+++ maintenance/fixStuckGlobalRename.php
@@ -0,0 +1,111 @@
+<?php
+
+use MediaWiki\Extension\CentralAuth\GlobalRename\LocalRenameJob\LocalRenameUserJob;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\WikiMap\WikiMap;
+use Wikimedia\Rdbms\SelectQueryBuilder;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+/**
+ * Script to run global renames that are stuck in the status table with "queued" or "in progress"
+ * but failed for whatever reason
+ */
+class FixStuckGlobalRename extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->requireExtension( 'CentralAuth' );
+		$this->addArg( 'oldname', 'Old name' );
+		$this->addArg( 'newname', 'New name' );
+		$this->addOption( 'logwiki', 'Wiki where the log entry exists', true, true );
+		$this->addOption( 'ignorestatus', 'Ignore rename status. Don\'t do this when the rename '
+			. 'jobs might still be running.' );
+		$this->addDescription( 'Unstuck global rename on a single wiki' );
+	}
+
+	public function execute() {
+		global $wgLocalDatabases;
+		$userNameUtils = MediaWikiServices::getInstance()->getUserNameUtils();
+
+		$oldName = $userNameUtils->getCanonical( $this->getArg( 0 ) );
+		$newName = $userNameUtils->getCanonical( $this->getArg( 1 ) );
+		if ( $oldName === false || $newName === false ) {
+			$this->fatalError( 'Invalid name' );
+		}
+
+		$logTitle = Title::newFromText( 'Special:CentralAuth' )->getSubpage( $newName );
+		$ca = new CentralAuthUser( $newName );
+		if ( !$ca->renameInProgressOn( WikiMap::getCurrentWikiId() ) ) {
+			$this->fatalError( "{$ca->getName()} does not have a rename in progress on this wiki." );
+		}
+
+		$dbr = wfGetDB( DB_REPLICA, [], $this->getOption( 'logwiki' ) );
+		$queryData = DatabaseLogEntry::getSelectQueryData();
+		$row = $dbr->newSelectQueryBuilder()
+			->tables( $queryData['tables'] )
+			->select( $queryData['fields'] )
+			->where( $queryData['conds'] )
+			->andWhere( [
+				'log_type' => 'gblrename',
+				'log_action' => 'rename',
+				'log_namespace' => NS_SPECIAL,
+				'log_title' => $logTitle->getDBkey(),
+			] )
+			->options( $queryData['options'] )
+			->orderBy( 'log_timestamp', SelectQueryBuilder::SORT_DESC )
+			->joinConds( $queryData['join_conds'] )
+			->caller( __METHOD__ )
+			->fetchRow();
+
+		// try to guess the options if the log record does not contain them
+		if ( $row ) {
+			$logEntry = DatabaseLogEntry::newFromRow( $row );
+			$renamer = $logEntry->getPerformerIdentity()->getName();
+			$comment = $logEntry->getComment();
+			$logParams = $logEntry->getParameters();
+			$this->output( "Using $renamer as the renamer.\n" );
+		} else {
+			$this->output( "Could not find log entry, falling back to system account\n" );
+			$renamer = 'Global rename script';
+			$comment = '';
+		}
+
+		$params = [
+			'from' => $oldName,
+			'to' => $newName,
+			'renamer' => $renamer,
+			'movepages' => false,
+			'suppressredirects' => false,
+			'reason' => $comment,
+			'ignorestatus' => $this->getOption( 'ignorestatus', false ),
+			// No way to recover localuser attachment details, faking it.
+			// $wgLocalDatabases will contain wikis where the user does not have an account.
+			// That's fine, LocalRenameUserJob will only use the one that matches WikiMap::getCurrentWikiId().
+			// FIXME the real attachment info should be saved & restored instead, see T215107
+			'reattach' => array_fill_keys( $wgLocalDatabases, [
+				'attachedMethod' => 'admin',
+				'attachedTimestamp' => wfTimestamp( TS_MW ),
+			] ),
+		];
+		foreach ( $params as $key => $value ) {
+			if ( $key === 'reattach' ) {
+				continue;
+			}
+			$this->output( "$key: $value\n" );
+		}
+		$title = Title::newFromText( 'Global rename job' );
+		$job = new LocalRenameUserJob( $title, $params );
+		$this->output( "\nStarting to run job...\n" );
+		$status = $job->run();
+		$job->teardown( $status );
+		$this->output( $status ? "Done!\n" : "Failed!\n" );
+	}
+}
+
+$maintClass = FixStuckGlobalRename::class;
+require_once RUN_MAINTENANCE_IF_MAIN;

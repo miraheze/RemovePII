4bff5e06c9e0e020c1cf445db88d1e99cd3346fd
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-04 13:39:42 -0600

    RemovePIIJob: cleanup and use QueryBuilder

--- .phan/config.php
+++ .phan/config.php
@@ -2,6 +2,8 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$cfg['minimum_target_php_version'] = '8.1';
+
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
 		'../../extensions/CentralAuth',
@@ -21,8 +23,6 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 $cfg['suppress_issue_types'] = [
 	'PhanAccessMethodInternal',
 	'SecurityCheck-LikelyFalsePositive',
-	// Issue with backwords compatible code (TODO: To be removed)
-	'PhanUndeclaredMethod',
 	'PhanDeprecatedFunction',
 ];
 
